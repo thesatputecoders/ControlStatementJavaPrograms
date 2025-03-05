d420b62ee49cac5a1eb3dbeff608449d0fd7ab1b
Jayesh Satpute <jayeshsatputehpl@gmail.com>
2025-03-06 00:12:09 +0530

    updated with the if else program

---
+++ src/IfElseProgramClass.java
@@ -0,0 +1,11 @@
+public class IfElseProgramClass {
+    public static void main(String []args){
+        int jay=25;
+        if(jay>=23){
+            System.out.println("jayesh can drive");
+        }
+        else{
+            System.out.println("System wont allow to drive");
+        }
+    }
+}
